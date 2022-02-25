13ca30070077f2e8899037186efe34d5d79fd4b5
Паша Луковников <lukovnikov.pasha@gmail.com>
2022-02-25 16:16:39 +1000

    ++Search and Pagination

--- MySklad/ViewModel/PersonalViewModel.cs
+++ MySklad/ViewModel/PersonalViewModel.cs
@@ -3,6 +3,7 @@ using MySklad.Core;
 using MySklad.View;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
