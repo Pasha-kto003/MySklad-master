16667da03e1badad0e4d9f9e8a7753b6928c189e
Паша Луковников <lukovnikov.pasha@gmail.com>
2022-03-25 11:19:18 +1000

    ++

--- MySklad/ViewModel/OrderOutViewModel.cs
+++ MySklad/ViewModel/OrderOutViewModel.cs
@@ -11,6 +11,76 @@ namespace MySklad.ViewModel
 {
     public class OrderOutViewModel : BaseViewModel
     {
+        public List<OrderOutApi> searchResult { get; set; }
+        public string SearchCountRows
+        {
+            get => searchCountRows;
+            set
+            {
+                searchCountRows = value;
+                SignalChanged();
+            }
+        }
+
+        private string searchText = "";
+        public string SearchText
+        {
+            get => searchText;
+            set
+            {
+                searchText = value;
+                Search();
+            }
+        }
+
+        public List<string> ViewCountRows { get; set; }
+        public string SelectedViewCountRows
+        {
+            get => selectedViewCountRows;
+            set
+            {
+                selectedViewCountRows = value;
+                paginationPageIndex = 0;
+                Pagination();
+            }
+        }
+
+        public List<string> SearchType { get; set; }
+        private string selectedSearchType;
+        public string SelectedSearchType
+        {
+            get => selectedSearchType;
+            set
+            {
+                selectedSearchType = value;
+                Search();
+            }
+        }
+
+        private string selectedOrderType;
+        public List<string> OrderType { get; set; }
+        public string SelectedOrderType
+        {
+            get => selectedOrderType;
+            set
+            {
+                selectedOrderType = value;
+                Sort();
+            }
+        }
+
+        public List<string> SortType { get; set; }
+        private string selectedSortType;
+        public string SelectedSortType
+        {
+            get => selectedSortType;
+
+            set
+            {
+                selectedSortType = value;
+                Sort();
+            }
+        }
         private List<OrderOutApi> orders { get; set; }
         public List<OrderOutApi> Orders
         {
@@ -77,10 +147,31 @@ namespace MySklad.ViewModel
                 order.Supplier = Suppliers.First(s => s.Id == order.SupplierId);
                 order.Shop = Shops.First(s => s.Id == order.ShopId);
             }
+            SignalChanged("Orders");
+            InitPagination();
         }
 
         public CustomCommand CreateOrderOut { get; set; }
         public CustomCommand EditOrderOut { get; set; }
+        public CustomCommand BackPage { get; set; }
+        public CustomCommand ForwardPage { get; set; }
+
+        int paginationPageIndex = 0;
+        private string searchCountRows;
+        private string selectedViewCountRows;
+        public int rows = 0;
+        public int CountPages = 0;
+
+        private string pages;
+        public string Pages
+        {
+            get => pages;
+            set
+            {
+                pages = value;
+                SignalChanged();
+            }
+        }
 
         public OrderOutViewModel()
         {
@@ -88,7 +179,48 @@ namespace MySklad.ViewModel
             Suppliers = new List<SupplierApi>();
             Orders = new List<OrderOutApi>();
             Shops = new List<ShopApi>();
-            
+            GetOrders();
+
+            ViewCountRows = new List<string>();
+            ViewCountRows.AddRange(new string[] { "5", "все" });
+            selectedViewCountRows = ViewCountRows.Last();
+
+            SortType = new List<string>();
+            SortType.AddRange(new string[] { "Дата" });
+            selectedSortType = SortType.First();
+
+            OrderType = new List<string>();
+            OrderType.AddRange(new string[] { "По возрастанию", "По убыванию", "По умолчанию" });
+            selectedOrderType = OrderType.Last();
+
+            SearchType = new List<string>();
+            SearchType.AddRange(new string[] { "Дата", "Поставщик", "Магазин" });
+            selectedSearchType = SearchType.First();
+
+            BackPage = new CustomCommand(() => {
+                if (searchResult == null)
+                    return;
+                if (paginationPageIndex > 0)
+                    paginationPageIndex--;
+                Pagination();
+
+            });
+
+            ForwardPage = new CustomCommand(() =>
+            {
+                if (searchResult == null)
+                    return;
+                int.TryParse(SelectedViewCountRows, out int rowsOnPage);
+                if (rowsOnPage == 0)
+                    return;
+                int countPage = searchResult.Count / rowsOnPage;
+                CountPages = countPage;
+                if (searchResult.Count() % rowsOnPage != 0)
+                    countPage++;
+                if (countPage > paginationPageIndex + 1)
+                    paginationPageIndex++;
+                Pagination();
+            });
 
             CreateOrderOut = new CustomCommand(() =>
             {
@@ -104,7 +236,72 @@ namespace MySklad.ViewModel
                 addOrder.ShowDialog();
                 GetOrders();
             });
-            GetOrders();
+            SignalChanged("Orders");
+            Search();
+            InitPagination();
+            Pagination();
+        }
+
+        private void InitPagination()
+        {
+            SearchCountRows = $"Найдено записей: {searchResult.Count} из {Orders.Count}";
+            paginationPageIndex = 0;
+        }
+
+        private void Pagination()
+        {
+            int rowsOnPage = 0;
+            if (!int.TryParse(SelectedViewCountRows, out rowsOnPage))
+            {
+                Orders = searchResult;
+            }
+            else
+            {
+                Orders = searchResult.Skip(rowsOnPage * paginationPageIndex).Take(rowsOnPage).ToList();
+                SignalChanged("Orders");
+                int.TryParse(SelectedViewCountRows, out rows);
+                CountPages = (searchResult.Count() - 1) / rows;
+                Pages = $"{paginationPageIndex + 1} из {CountPages + 1}";
+            }
+        }
+
+        internal void Sort()
+        {
+            if (SelectedOrderType == "По умолчанию")
+                return;
+
+            if (SelectedOrderType == "По убыванию")
+            {
+                if (SelectedSortType == "Дата")
+                    searchResult.Sort((x, y) => y.DateOrderOut.ToString().CompareTo(x.DateOrderOut));
+            }
+
+            if (SelectedOrderType == "По возрастанию")
+            {
+                if (SelectedSortType == "Дата")
+                    searchResult.Sort((x, y) => x.DateOrderOut.ToString().CompareTo(y.DateOrderOut));
+            }
+            paginationPageIndex = 0;
+
+            Pagination();
+            SignalChanged("Orders");
+        }
+
+        private void Search()
+        {
+            var search = SearchText.ToLower();
+            if (SelectedSearchType == "Дата")
+                searchResult = Orders
+                    .Where(c => c.DateOrderOut.ToString().ToLower().Contains(search)).ToList();
+            else if (SelectedSearchType == "Поставщик")
+                searchResult = Orders
+                        .Where(c => c.Supplier.Title.ToLower().Contains(search)).ToList();
+            else if (SelectedSearchType == "Магазин")
+                searchResult = Orders.Where(c => c.Shop.Name.ToLower().Contains(search)).ToList();
+            Sort();
+            InitPagination();
+            Pagination();
+            SignalChanged("Orders");
         }
     }
 }
